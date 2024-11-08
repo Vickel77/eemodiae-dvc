c221ffb5ff84c531bc3f2bdb899f633b4a81b448
vickel77 <kelechialigwo77@gmail.com>
2024-11-08 09:39:59 +0100

    modified export button

--- src/components/NextPage.jsx
+++ src/components/NextPage.jsx
@@ -102,99 +102,93 @@ const App = () => {
   };
   return (
     <>
-    
-    
-
-       
-    
-    <div ref={componentRef} className="flex w-full items-center">
-      <SideBar />
-      <div className="heroImg text-white p-3 px-8 rounded-lg shadow-lg w-1/2 mx-auto my-auto">
-        {/* Header Section */}
-        <Header />
-        <div className="  w-flex ">
-          <div className="flex justify-between  items-center mt-8">
-            <div className="flex  h-full">
-              {tabs.map((tab, index) => (
-                <div
-                  key={tab.id}
-                  className={`h-1 w-8 mx-1 ${
-                    index === activeTab ? "bg-[#1FF8F2]" : "bg-gray-400"
-                  }`}
-                  style={{
-                    borderRadius: index === activeTab ? "4px" : "4px",
-                    height: "4px",
-                  }}
-                />
-              ))}
+      <div ref={componentRef} className="flex w-full items-center">
+        <SideBar />
+        <div className="heroImg text-white p-3 px-8 rounded-lg shadow-lg w-1/2 mx-auto my-auto">
+          {/* Header Section */}
+          <Header />
+          <div className="  w-flex ">
+            <div className="flex justify-between  items-center mt-8">
+              <div className="flex  h-full">
+                {tabs.map((tab, index) => (
+                  <div
+                    key={tab.id}
+                    className={`h-1 w-8 mx-1 ${
+                      index === activeTab ? "bg-[#1FF8F2]" : "bg-gray-400"
+                    }`}
+                    style={{
+                      borderRadius: index === activeTab ? "4px" : "4px",
+                      height: "4px",
+                    }}
+                  />
+                ))}
+              </div>
             </div>
-
-          </div>
-          <div className="">
-            <TabContent tab={tabs[activeTab]} />
-            {/* Page Counter */}
-            <div className="flex justify-between items-center mt2">
-              <span className="linear rounded-[10px] px-[20px] text-[20px] quicksand py-2 hover:bg-green-500 text-white">
-                {activeTab + 1}/{tabs.length}
-              </span>
-              {/* Navigation Buttons */}
-              <div className="space-x-2">
-                <button
-                  className=" rounded-[10px] bg-[#033331bb] px-[20px] text-[20px] quicksand py-2  text-white"
-                  onClick={() => setActiveTab((activeTab - 1) % tabs.length)}
-                >
-                  <IoChevronBackOutline />
-                </button>
-                <button
-                  className="rounded-[10px] px-[20px] bg-[#033331bb] text-[20px] quicksand py-2  text-white"
-                  onClick={() => setActiveTab((activeTab + 1) % tabs.length)}
-                >
-                  <IoChevronForwardOutline />
-                </button>
+            <div className="">
+              <TabContent tab={tabs[activeTab]} />
+              {/* Page Counter */}
+              <div className="flex justify-between items-center mt2">
+                <span className="linear rounded-[10px] px-[20px] text-[20px] quicksand py-2 hover:bg-green-500 text-white">
+                  {activeTab + 1}/{tabs.length}
+                </span>
+                {/* Navigation Buttons */}
               </div>
             </div>
           </div>
-        </div>
 
-        {/* Confession Section */}
+          {/* Confession Section */}
 
-        {/* Footer Section */}
-        <div className="flex justify-between items-center mt-6">
-          <div>
-            <img src={HouseJoy} className="w-40" alt="" />
-          </div>
-          <div className="flex flex-col items-center">
-            <img src={vector} alt="" />
-            <h3 className="quicksand">eemodioe.org</h3>
-          </div>
-          <div className="flex flex-col items-center">
-            <div className="flex">
-              <img src={Vector2} alt="" />
-              <img src={Vector3} alt="" />
+          {/* Footer Section */}
+          <div className="flex justify-between items-center mt-6">
+            <div>
+              <img src={HouseJoy} className="w-40" alt="" />
             </div>
-            <h3>hjcglobal</h3>
-          </div>
-          <div>
-            <Button
-              text="Please Share"
-              className="bg-[#1FF8F2] text-[#000000] font-[700] rounded-[20px] px-[20px] text-[20px] quicksand py-4 "
-            />
+            <div className="flex flex-col items-center">
+              <img src={vector} alt="" />
+              <h3 className="quicksand">eemodioe.org</h3>
+            </div>
+            <div className="flex flex-col items-center">
+              <div className="flex">
+                <img src={Vector2} alt="" />
+                <img src={Vector3} alt="" />
+              </div>
+              <h3>hjcglobal</h3>
+            </div>
+            <div>
+              <Button
+                text="Please Share"
+                className="bg-[#1FF8F2] text-[#000000] font-[700] rounded-[20px] px-[20px] text-[20px] quicksand py-4 "
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-         <p
-              className="italic cursor-pointer quicksand linearColor"
-              onClick={() =>
-                exportComponentAsPNG(componentRef, {
-                  quality: 1,
-                  scale: 100, // Increase scale to improve clarity
-                  backgroundColor: "#ffffff", // Sets a solid white background to avoid transparency
-                })
-              }
-            >
-              Export as PNG
-            </p>
+      <div className="space-x-2">
+        <button
+          className=" rounded-[10px] bg-[#033331bb] px-[20px] text-[20px] quicksand py-2  text-white"
+          onClick={() => setActiveTab((activeTab - 1) % tabs.length)}
+        >
+          <IoChevronBackOutline />
+        </button>
+        <button
+          className="rounded-[10px] px-[20px] bg-[#033331bb] text-[20px] quicksand py-2  text-white"
+          onClick={() => setActiveTab((activeTab + 1) % tabs.length)}
+        >
+          <IoChevronForwardOutline />
+        </button>
+      </div>
+      <p
+        className="italic cursor-pointer quicksand linearColor"
+        onClick={() =>
+          exportComponentAsPNG(componentRef, {
+            quality: 1,
+            scale: 100, // Increase scale to improve clarity
+            backgroundColor: "#ffffff", // Sets a solid white background to avoid transparency
+          })
+        }
+      >
+        Export as PNG
+      </p>
     </>
   );
 };
